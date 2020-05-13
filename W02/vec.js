3aeb35d090bd49e450055f8933f32fc91d06a14f
zhangyihao91 <65213261+zhangyihao91@users.noreply.github.com>
2020-05-13 21:17:17 +0900

    Create vec.js

---
+++ W02/vec.js
@@ -0,0 +1,35 @@
+class Vec3 {
+  // Constructor
+  constructor(x, y, z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+
+  // Add method
+  add(v) {
+    this.x += v.x;
+    this.y += v.y;
+    this.z += v.z;
+  }
+
+  // Sum method
+  sum() {
+    return this.x + this.y + this.z;
+  }
+
+  //Max method
+  max() {
+    return Math.max(this.x, this.y, this.z);
+  }
+  
+  // Min method
+  min() {
+    return Math.min(this.x, this.y, this.z);
+  }
+
+  // Mid method
+  mid() {
+    return this.x + this.y + this.z - min(this.x, this.y, this.z) - max(this.x, this.y, this.z);
+  }
+}
